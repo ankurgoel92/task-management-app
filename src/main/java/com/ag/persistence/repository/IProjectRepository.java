88915eea3b0d7b80cbcc7e89779350320507946e
ankurgoel92 <ankurgoel.juit@gmail.com>
2019-07-21 18:41:54 +0530

    04. C. Adding Paging and Sorting Repository and tests
    04. C. Adding Paging and Sorting Repository and tests

--- src/main/java/com/ag/persistence/repository/IProjectRepository.java
+++ src/main/java/com/ag/persistence/repository/IProjectRepository.java
@@ -1,14 +1,13 @@
 package com.ag.persistence.repository;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.ag.persistence.model.Project;
 
-public interface IProjectRepository extends CrudRepository<Project, Long> {
+public interface IProjectRepository extends PagingAndSortingRepository<Project, Long> {
     
     Optional<Project> findByName(String name);
     
--- src/test/java/com/ag/persistence/repository/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/ag/persistence/repository/ProjectRepositoryIntegrationTest.java
@@ -1,21 +1,27 @@
-package com.ag.repository;
+package com.ag.persistence.repository;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 
 import com.ag.persistence.model.Project;
-import com.ag.persistence.repository.IProjectRepository;
 
 @SpringBootTest
 class ProjectRepositoryIntegrationTest {
@@ -61,8 +67,34 @@ class ProjectRepositoryIntegrationTest {
         projectRepository.save(newProject2);
 
         List<Project> retreivedProjects = projectRepository.findByDateCreatedBetween(LocalDate.now().minusDays(1), LocalDate.now().plusDays(1));
-        
+
         assertThat(retreivedProjects, hasItems(newProject, newProject2));
+    }
+
+    @Test
+    public void givenProjects_whenFindAllPaginated_thenSuccess() {
+        Page<Project> retreivedProjects = projectRepository.findAll(PageRequest.of(0, 2));
+
+        assertThat(retreivedProjects.getContent(), hasSize(2));
+    }
+
+    @Test
+    public void givenProjects_whenFindAllSorted_thenSuccess() {
+
+        List<Project> retreivedProjects = (List<Project>) projectRepository.findAll(Sort.by(Order.asc("name")));
+
+        List<Project> projectList = retreivedProjects.stream().collect(Collectors.toList());
+        projectList.sort(Comparator.comparing(Project::getName));
+
+        assertEquals(retreivedProjects, projectList);
+
+    }
+    
+    @Test
+    public void givenProjects_whenFindPaginatedAndSortedProjects_thenSuccess() {
+        Page<Project> retreivedProjects = projectRepository.findAll(PageRequest.of(0, 2, Sort.by(Order.asc("name"))));
+        
+        assertThat(retreivedProjects.getContent(), hasSize(2));
     }
 
 }
